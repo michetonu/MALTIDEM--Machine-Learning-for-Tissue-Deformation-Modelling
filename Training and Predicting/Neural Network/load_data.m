8de83dafc72b284eddad79a7e97605de9b2d6acc
Michele Tonutti <michetonu@gmail.com>
2017-06-04 13:10:59 +0200

    Cleaned functions

--- Training and Predicting/Neural Network/load_data.m
+++ Training and Predicting/Neural Network/load_data.m
@@ -1,18 +1,14 @@
-function [nn_input,nn_input_compare,nn_output,nn_output_compare] = SVM_load_data(force_node_compare,node_choice)
+function [nn_input, nn_output, nn_input_compare, nn_output_compare] = load_data(force_node_compare,node_choice)
 
 iterations = load('iterations.mat');
 coordinates = load('coordinates.mat');
 nodes_list = load('nodes_list.mat');
 force_nodes = load('force_nodes.mat');
 
-%force_node_compare = 82;
 force_nodes = force_nodes(force_nodes(:,1)~=force_node_compare,1);
 fn = 20;
-nn_input = NaN;
-nn_output = NaN;
+
 a=1;
-vectemp = NaN;
-th = NaN;
 
 for i = 1:size(force_nodes,1)
     force_node = force_nodes(i);
@@ -22,19 +18,18 @@ for i = 1:size(force_nodes,1)
     for z = 1:30
         for n = node_choice
             for f = 1:fn
-                %if iterations{z}.cos_plot(n,f)>0
-                nn_input(a,1) = iterations{z}.cos_plot(n,f);
                 for g =1:3
                     nn_input(a,g+1) = iterations{z}.force_direction(f,g);
                 end
-                nn_input(a,5) = iterations{z}.dist_plot(n,f);
+                nn_input(a,5) = iterations{1}.dist_plot(n,f);
                 
+                th = zeros(3);
                 for h=1:3
                     th(h) = iterations{z}.force_direction(fn,h)/norm(iterations{z}.force_direction(fn,:));
                     th(h) = acos(th(h));
                     nn_input(a,5+h) = th(h);
                 end
-                
+                vectemp = zeros(2,3);
                 for xyz = 1:3
                     vectemp(1,xyz) = coordinates(force_node,xyz+1,1);
                     vectemp(2,xyz) = cmbrain(xyz);
@@ -45,9 +40,14 @@ for i = 1:size(force_nodes,1)
                 nn_input(a,10) = r(1);
                 nn_input(a,11) = r(2);
                 nn_input(a,12) = r(3);
+                nn_input(a,13) = r(4);
                 
-                nn_output(a,1) = iterations{z}.displ_tot_plot(n,f);
-                
+                for gg = 1:3
+                    tho = iterations{z}.displ_max_vector{n,f}(gg)/norm(iterations{z}.displ_max_vector{n,f}(:));
+                    tho = acos(tho);
+                    
+                    nn_output(a,gg) = tho;
+                end
                 
                 a=a+1;
                 %end
@@ -57,52 +57,50 @@ for i = 1:size(force_nodes,1)
     a
 end
 
-
-vectemp = NaN;
-th = NaN;
 nn_input_compare = NaN;
 nn_output_compare = NaN;
 a=1;
-z=1;
-dir_name = strcat('/Volumes/Macintosh HD/Users/MicTonutti/Documents/Imperial/MRes/Individual Project 2/Nodewise Simulations/Node',num2str(force_node_compare));
+root = 'user_defined_root';
+dir_name = strcat(root, '/Node',num2str(force_node_compare));
 
 var_filename = strcat(dir_name,'/All_Nodes_Iterations_Fn',num2str(force_node_compare),'.mat');
 load(var_filename);
 for z = 1:30
-for n = node_choice
-    for f = 1:fn
-        %if iterations{z}.cos_plot(n,f)>0
+    for n = node_choice
+        for f = 1:fn
             nn_input_compare(a,1) = iterations{z}.cos_plot(n,f);
             for g =1:3
-            nn_input_compare(a,g+1) = iterations{z}.force_direction(f,g);
+                nn_input_compare(a,g+1) = iterations{z}.force_direction(f,g);
             end
-            nn_input_compare(a,5) = iterations{z}.dist_plot(n,f);
+            nn_input_compare(a,5) = iterations{1}.dist_plot(n,f);
             
             for h=1:3
-            th(h) = iterations{z}.force_direction(fn,h)/norm(iterations{z}.force_direction(fn,:));
-            th(h) = acos(th(h));
-            nn_input_compare(a,5+h) = th(h);          
+                th(h) = iterations{z}.force_direction(fn,h)/norm(iterations{z}.force_direction(fn,:));
+                th(h) = acos(th(h));
+                nn_input_compare(a,5+h) = th(h);
             end
             
             for xyz = 1:3
-                vectemp(1,xyz) = coordinates(force_node_compare,xyz+1,1);
+                vectemp(1,xyz) = DATA_coord(force_node_compare,xyz+1,1);
                 vectemp(2,xyz) = cmbrain(xyz);
             end
             nn_input_compare(a,9) = pdist(vectemp);
             
-            r = vrrotvec(coordinates(force_node_compare,2:4,1)-coordinates(nodes_list(n),2:4,1),iterations{z}.force_direction(fn,:));
+            r = vrrotvec(DATA_coord(force_node_compare,2:4,1)-DATA_coord(nodes_list(n),2:4,1),iterations{z}.force_direction(fn,:));
             nn_input_compare(a,10) = r(1);
             nn_input_compare(a,11) = r(2);
             nn_input_compare(a,12) = r(3);
-
+            nn_input_compare(a,13) = r(4);
             
-            nn_output_compare(a,1) = iterations{z}.displ_tot_plot(n,f);
-            
-            
+            for gg = 1:3
+                tho = iterations{z}.displ_max_vector{n,f}(gg)/norm(iterations{z}.displ_max_vector{n,f}(:));
+                tho = acos(tho);
+                
+                nn_output_compare(a,gg) = tho;
+            end
             a=a+1;
-        %end
+        end
     end
-end
 end
 
 end
--- Training and Predicting/SVM/SVM_load_data.m
+++ Training and Predicting/SVM/SVM_load_data.m
@@ -1,16 +1,17 @@
-function [nn_input, nn_output, nn_input_compare, nn_output_compare] = load_data(iterations,coordinates,nodes_list,force_nodes,force_node_compare,node_choice)
+function [nn_input,nn_input_compare,nn_output,nn_output_compare] = SVM_load_data(force_node_compare,node_choice)
 
-%load('/Volumes/Macintosh HD/Users/MicTonutti/Documents/Imperial/MRes/Individual Project 2/Nodewise Simulations/Node101/Variables1.mat','DATA_coord','nodes_list');
+iterations = load('iterations.mat');
+coordinates = load('coordinates.mat');
+nodes_list = load('nodes_list.mat');
+force_nodes = load('force_nodes.mat');
 
-%force_nodes = [82;91;101;107;129;140;148;163;174;185;203];
-%force_node_compare = 82;
 force_nodes = force_nodes(force_nodes(:,1)~=force_node_compare,1);
 fn = 20;
 nn_input = NaN;
 nn_output = NaN;
-%node_choice = 10;
 a=1;
-err = [];
+vectemp = NaN;
+th = NaN;
 
 for i = 1:size(force_nodes,1)
     force_node = force_nodes(i);
@@ -21,10 +22,11 @@ for i = 1:size(force_nodes,1)
         for n = node_choice
             for f = 1:fn
                 %if iterations{z}.cos_plot(n,f)>0
-                    for g =1:3
+                nn_input(a,1) = iterations{z}.cos_plot(n,f);
+                for g =1:3
                     nn_input(a,g+1) = iterations{z}.force_direction(f,g);
                 end
-                nn_input(a,5) = iterations{1}.dist_plot(n,f);
+                nn_input(a,5) = iterations{z}.dist_plot(n,f);
                 
                 for h=1:3
                     th(h) = iterations{z}.force_direction(fn,h)/norm(iterations{z}.force_direction(fn,:));
@@ -42,15 +44,10 @@ for i = 1:size(force_nodes,1)
                 nn_input(a,10) = r(1);
                 nn_input(a,11) = r(2);
                 nn_input(a,12) = r(3);
-                nn_input(a,13) = r(4);
                 
-                for gg = 1:3
-                tho = iterations{z}.displ_max_vector{n,f}(gg)/norm(iterations{z}.displ_max_vector{n,f}(:));
-                tho = acos(tho);
+                nn_output(a,1) = iterations{z}.displ_tot_plot(n,f);
                 
-                nn_output(a,gg) = tho;
-                end
-               
+                
                 a=a+1;
                 %end
             end
@@ -59,50 +56,52 @@ for i = 1:size(force_nodes,1)
     a
 end
 
+
+vectemp = NaN;
+th = NaN;
 nn_input_compare = NaN;
 nn_output_compare = NaN;
 a=1;
-root = 'user_defined_root';
-dir_name = strcat(root, '/Node',num2str(force_node_compare));
+z=1;
+dir_name = strcat('/Volumes/Macintosh HD/Users/MicTonutti/Documents/Imperial/MRes/Individual Project 2/Nodewise Simulations/Node',num2str(force_node_compare));
 
 var_filename = strcat(dir_name,'/All_Nodes_Iterations_Fn',num2str(force_node_compare),'.mat');
 load(var_filename);
 for z = 1:30
-    for n = node_choice
-        for f = 1:fn
+for n = node_choice
+    for f = 1:fn
+        %if iterations{z}.cos_plot(n,f)>0
             nn_input_compare(a,1) = iterations{z}.cos_plot(n,f);
             for g =1:3
-                nn_input_compare(a,g+1) = iterations{z}.force_direction(f,g);
+            nn_input_compare(a,g+1) = iterations{z}.force_direction(f,g);
             end
-            nn_input_compare(a,5) = iterations{1}.dist_plot(n,f);
+            nn_input_compare(a,5) = iterations{z}.dist_plot(n,f);
             
             for h=1:3
-                th(h) = iterations{z}.force_direction(fn,h)/norm(iterations{z}.force_direction(fn,:));
-                th(h) = acos(th(h));
-                nn_input_compare(a,5+h) = th(h);
+            th(h) = iterations{z}.force_direction(fn,h)/norm(iterations{z}.force_direction(fn,:));
+            th(h) = acos(th(h));
+            nn_input_compare(a,5+h) = th(h);          
             end
             
             for xyz = 1:3
-                vectemp(1,xyz) = DATA_coord(force_node_compare,xyz+1,1);
+                vectemp(1,xyz) = coordinates(force_node_compare,xyz+1,1);
                 vectemp(2,xyz) = cmbrain(xyz);
             end
             nn_input_compare(a,9) = pdist(vectemp);
             
-            r = vrrotvec(DATA_coord(force_node_compare,2:4,1)-DATA_coord(nodes_list(n),2:4,1),iterations{z}.force_direction(fn,:));
+            r = vrrotvec(coordinates(force_node_compare,2:4,1)-coordinates(nodes_list(n),2:4,1),iterations{z}.force_direction(fn,:));
             nn_input_compare(a,10) = r(1);
             nn_input_compare(a,11) = r(2);
             nn_input_compare(a,12) = r(3);
-            nn_input_compare(a,13) = r(4);
+
             
-            for gg = 1:3
-                tho = iterations{z}.displ_max_vector{n,f}(gg)/norm(iterations{z}.displ_max_vector{n,f}(:));
-                tho = acos(tho);
-                
-                nn_output_compare(a,gg) = tho;
-            end
+            nn_output_compare(a,1) = iterations{z}.displ_tot_plot(n,f);
+            
+            
             a=a+1;
-        end
+        %end
     end
+end
 end
 
 end
