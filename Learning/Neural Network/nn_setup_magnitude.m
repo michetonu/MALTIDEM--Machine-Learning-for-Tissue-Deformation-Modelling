974a90a01157d772323b100395c288674d387352
Michele Tonutti <michetonu@gmail.com>
2017-06-05 11:26:18 +0200

    Updated hyper parameters in function declarations

--- Learning/Neural Network/nn_setup_magnitude.m
+++ Learning/Neural Network/nn_setup_magnitude.m
@@ -9,11 +9,13 @@ force_node_compare = 91;
 force_nodes = force_nodes(force_nodes(:,1)~=force_node_compare,1);
 %Number of force steps (from 0 to 1N)
 fn = 20;
+%Size of hidden layer
+hiddenLayerSize = 4;
 
 [nn_input, nn_output, nn_input_compare, nn_output_compare] = load_data(iterations,coordinates,nodes_list,force_nodes,force_node_compare,node_choice);
 
-[aaaa,y_dir,e] = neural_net_direction(nn_input(:,2:end),nn_output,nn_input_compare(:,2:end),nn_output_compare);
-e
-estd = std(aaaa)
-err = [err e]
+[accuracy,y,abs_mean_acc,performance] = neural_net_magnitude(nn_input,nn_output,nn_input_compare,nn_output_compare,hiddenLayerSize);
+abs_mean_acc
+estd = std(accuracy)
+err = [err abs_mean_acc]
 performance
